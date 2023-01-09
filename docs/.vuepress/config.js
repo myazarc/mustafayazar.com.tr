8517ddde6c5d9a3a7e7682609ff381a338e7b19f
myazarc <myazarc@yandex.com>
2023-01-09 19:30:16 +0300

    title

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+import { defineUserConfig } from "vuepress";
+
+export default defineUserConfig({
+  lang: "tr-TR",
+  title: "MustafaYazar | Blog",
+  description: "Mustafa Yazar Karalama Defteri",
+});
